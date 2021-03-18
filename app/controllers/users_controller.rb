92f42b15bef67acfcb64e896672364bc085153cc
Ryel Banfield <ryelbanfield@gmail.com>
2021-03-18 11:13:52 -0400

    Railsy Forms with form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(whitelisted_user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(whitelisted_user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_user_path
     else
       render :new
     end
